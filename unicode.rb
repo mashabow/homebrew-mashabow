4c4a2df6f08f1a130e2ac298fe9090a8ce8f6f9c
mashabow <mashabow@shiromoji.net>
2015-06-19 10:33:40 +0900

    Update unicode character database to v8.0.0

--- unicode.rb
+++ unicode.rb
@@ -2,17 +2,17 @@ class Unicode < Formula
   desc "Display unicode character properties"
   homepage "http://kassiopeia.juls.savba.sk/~garabik/software/unicode.html"
   url "https://github.com/garabik/unicode/archive/v1.zip"
-  version "1"
   sha256 "f0f7818bbdfccd62e2d2d78ff31f34d8f9b2add068b413ecbe62c7a52a6bc2af"
+  revision 1
 
   resource "UnicodeData" do
-    url "http://www.unicode.org/Public/7.0.0/ucd/UnicodeData.txt"
-    sha256 "bfa3da58ea982199829e1107ac5a9a544b83100470a2d0cc28fb50ec234cb840"
+    url "http://www.unicode.org/Public/8.0.0/ucd/UnicodeData.txt"
+    sha256 "38b17e1118206489a7e0ab5d29d7932212d38838df7d3ec025ecb58e8798ec20"
   end
 
   resource "Unihan" do
-    url "http://www.unicode.org/Public/7.0.0/ucd/Unihan.zip"
-    sha256 "a012d564130da81dc789de4d58f6af5fb9a8f6cce31338418fb7726a79bc5e6c"
+    url "http://www.unicode.org/Public/8.0.0/ucd/Unihan.zip"
+    sha256 "e02affdd082ca9365f595e5dfa3295dc9e0bf7f6aed54455d99c0a9924b88115"
   end
 
   def install
